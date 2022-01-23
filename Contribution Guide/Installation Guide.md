d2f5f98227e9e0647e64a21e9dd14e8aa887334f
Mukul Jain <mukul.jain1906@yahoo.com>
2022-01-23 15:52:54 +0530

    Minor fixes

--- Contribution Guide/Installation Guide.md
+++ Contribution Guide/Installation Guide.md
@@ -2,13 +2,13 @@
 
 ### Requirements
 1. Python 3.6+
-2. Pip installed
+2. Pip  
 3. Basic Knowledge of Virtual Environment
 
 ### Steps to Reproduce
 1. Clone the repository.
 2. If you are a Linux user:
-3. Go into the Linux directory.
+3. Go into the Linux Users directory.
 4. `chmod +x setup.sh`
 5. `source ./setup.sh`
 6. To run the Application, go into the parent directory.
--- createapp.py
+++ createapp.py
@@ -3,6 +3,9 @@ directory = sys.argv[1]
 path = os.path.join(os.getcwd(), directory)
 os.mkdir(path)
 
+with open(os.path.join(path, "__init__.py"), "w") as f:
+    pass
+
 with open(os.path.join(path, "models.py"), "w") as f:
     f.write("from Server.connection import Base\n")
     f.write("# Add the above Base to migrations/env.py\n\n")
@@ -13,6 +16,6 @@ with open(os.path.join(path, "schemas.py"), "w") as f:
 with open(os.path.join(path, "router.py"), "w") as f:
     f.write("# Add this router to main.py\n\n")
 
-print(directory, "created!")
+print(directory, "App created!")
 
 # This file can be deleted after the project is completed.
