df6b33ada3a52ce2d00bf3eb2b715eeaa888aba1
Mukul Jain <mukul.jain1906@yahoo.com>
2022-01-25 10:28:16 +0530

    Minor changes

--- Contribution Guide/Linux Users/setup.sh
+++ Contribution Guide/Linux Users/setup.sh
@@ -7,9 +7,9 @@ echo "Virtual Environment Created"
 
 source venv/bin/activate
 pip install -r requirements.txt
-echo "Install Required Packages"
+echo "Installed Required Packages"
 
 touch .env
-printf "IN=DEVELOPMENT\nDATABASE_USERNAME=\nDATABASE_PASSWORD=\nDATABASE=\nSECRET_KEY=\nGOOGLE_CLIENT_ID=\nGOOGLE_CLIENT_SECRET=\nFRONTEND_URL=" >> .env
+printf "ENVIRONMENT=DEVELOPMENT\nDATABASE_USERNAME=\nDATABASE_PASSWORD=\nDATABASE=\nSECRET_KEY=\nGOOGLE_CLIENT_ID=\nGOOGLE_CLIENT_SECRET=\nFRONTEND_URL=" >> .env
 echo "Fill the credentials in .env file"
 echo "You are now in the parent directory"
--- migrations/env.py
+++ migrations/env.py
@@ -22,7 +22,7 @@ target_metadata = [Base.metadata]
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-config.set_main_option("sqlalchemy.url", settings.CONNECTION_STRING)
+config.set_main_option("sqlalchemy.url", settings.get_connection_string())
 
 
 def run_migrations_offline():
