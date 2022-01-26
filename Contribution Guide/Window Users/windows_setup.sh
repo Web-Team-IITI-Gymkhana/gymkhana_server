532844c6233d7ef4db2c6fee51e884780f6f2eca
new usere <cse200001056@iiti.ac.in>
2022-01-27 03:37:46 +0530

    modify windows

--- Contribution Guide/Window Users/windows_setup.sh
+++ Contribution Guide/Window Users/windows_setup.sh
@@ -1,7 +1,7 @@
-cd gymkhana_server
+cd../..
 pip install virtualenv
 virtualenv venv
 # echo "Virtual Environment Created"
 source venv/Scripts/activate.bat
-pip install -r cd..windows_requirements.txt
+pip install -r windows_requirements.txt
 # echo "Installed Required Packages"  
