e2ccdb2a894f5e612611fbe8918af4d54a74767a
Mukul Jain <mukul.jain1906@yahoo.com>
2022-01-27 02:24:22 +0530

    Setup configured

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup, find_packages
+from server.config import settings
 
-setup(name="gymkhana_server", packages=find_packages())
+setup(
+    name="gymkhana_server",
+    version=settings.VERSION,
+    description=settings.DESCRIPTION,
+    author=settings.NAME,
+    author_email=settings.EMAIL,
+    url=settings.FRONTEND_URL,
+    license=settings.LICENSE_NAME,
+    packages=find_packages(),
+)
