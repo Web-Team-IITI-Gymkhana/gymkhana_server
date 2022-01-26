f4afa627fe42e61cb84f326ea581c7d60a2156af
new usere <cse200001056@iiti.ac.in>
2022-01-27 03:30:57 +0530

    added automatic setup for windows users (partially)

---
+++ Contribution Guide/Window Users/windows_setup.sh
@@ -0,0 +1,7 @@
+cd gymkhana_server
+pip install virtualenv
+virtualenv venv
+# echo "Virtual Environment Created"
+source venv/Scripts/activate.bat
+pip install -r cd..windows_requirements.txt
+# echo "Installed Required Packages"  
