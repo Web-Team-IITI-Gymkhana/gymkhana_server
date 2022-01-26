b678c8c6d4f55f08593eef0ae3013617b5457317
new usere <cse200001056@iiti.ac.in>
2022-01-26 19:58:09 +0530

    removed warden from hostel

--- hostel/schemas.py
+++ hostel/schemas.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 class HostelSchema(BaseModel):
     id: UUID
     name: str
-    warden: Optional[str] = None
+    # warden: Optional[str] = None
 
     class Config:
         orm_mode = True
@@ -15,6 +15,6 @@ class HostelSchema(BaseModel):
             "example": {
                 "id": "7e4d6231-85ef-45e1-af1d-69e1f42b29c1",
                 "name": "Devi Ahilya",
-                "warden": "John Doe",
+                # "warden": "John Doe",
             }
         }
