4c1bc16909871f24c782c10dbaea7bf7bf07198e
new usere <cse200001056@iiti.ac.in>
2022-01-27 17:01:59 +0530

    updated windows version

--- Contribution Guide/Window Users/windows_setup.sh
+++ Contribution Guide/Window Users/windows_setup.sh
@@ -1,7 +1,7 @@
-cd../..
+cd ../..
 pip install virtualenv
 virtualenv venv
-# echo "Virtual Environment Created"
-source venv/Scripts/activate.bat
+echo "Virtual Environment Created"
+venv\Scripts\activate
 pip install -r windows_requirements.txt
-# echo "Installed Required Packages"  
+echo "Installed Required Packages"  
